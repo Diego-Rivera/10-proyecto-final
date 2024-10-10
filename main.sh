777dbdf6ec487f730c83339c2be38f1b59f76a1b
Diego Rivera Ramírez <drivera@economia.cl>
2024-10-10 12:28:53 -0300

    FEATURE-008: Incorporación de función Message

--- main.sh
+++ main.sh
@@ -19,3 +19,9 @@ hola_mundo () {
 echo “¡Hola, mundo!”
 echo “Esta es una variable de prueba”
 }
+
+#Define a print message function
+print_message () {
+echo “Aprendamos programación Bash”
+echo “Disfruta este tutorial”
+}
